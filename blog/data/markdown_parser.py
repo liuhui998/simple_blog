82e2b7dca6cdcfc8677390f2986a07e5406abadb
Peter.Liu <liuhui998@gmail.com>
2022-09-28 08:23:15 +0800

    Finished blog parser class

--- blog/data/markdown_parser.py
+++ blog/data/markdown_parser.py
@@ -1,16 +1,25 @@
 from pprint import pprint
 from pathlib import Path
 import marko
+import os
 
 
 class PeterMarkdownParser:
 
-    def __init__(self, text):
-        self.text = text.strip()
+    def __init__(self, text=None, file_path=None):
         self.title = ""
         self.categories = []
         self.html_body = ""
         self.markdown_body = ""
+        self.date = None
+
+        if text:
+            self.text = text.strip()
+        elif file_path:
+            self.load_from_file(path)
+        else:
+            raise Exception("text or file_path should be specified either!")
+        self.parse()
 
     def load_from_file(self, path):
         self.text = Path(path).read_text().strip()
@@ -29,17 +38,36 @@ class PeterMarkdownParser:
                 second_occur_hr = index
                 break
 
+        self.parse_header("\n".join(blog_lines[1:second_occur_hr]))
         self.markdown_body = "\n".join(blog_lines[second_occur_hr + 1:])
         self.html_body = marko.convert(self.markdown_body)
 
+    def parse_header(self, header_text):
+        arr = list((item.split(": ") for item in header_text.split("\n")))
+        res = dict(arr)
+        self.categories = res['categories']
+        self.title = res['title']
+        self.date = res['date']
+
+    def __str__(self):
+        msg = """
+        title: %s,
+        date: %s,
+        categories: %s,
+        markdown_body: %s,
+        html_body: %s
+        """ % (self.title, self.date, str(
+            self.categories), self.markdown_body[0:50], self.html_body[0:50])
+        return msg
+
 
 if __name__ == "__main__":
-    p = PeterMarkdownParser("")
-    p.load_from_file(
-        "/Users/peter/code/django/simple_blog/blog/data/markdown/2020-08-28-do-not-be-anxious.markdown"
-    )
-    p.parse()
-    pprint(p.title)
-    pprint(p.categories)
-    pprint(p.html_body)
-    pprint(p.markdown_body)
+    base_dir = Path(__file__).resolve().parent
+    path = os.path.join(base_dir, "markdown",
+                        "2020-08-28-do-not-be-anxious.markdown")
+
+    p = PeterMarkdownParser(file_path=path)
+    print(p)
+
+    #pprint(p.html_body)
+    #pprint(p.markdown_body)
